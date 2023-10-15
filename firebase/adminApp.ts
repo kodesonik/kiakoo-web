27702ce35d68ae008cf339fcf807d1ac2eb3eb84
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-10-15 16:04:59 +0000

    Initial commit

--- firebase/adminApp.ts
+++ firebase/adminApp.ts
@@ -4,7 +4,7 @@ import * as firebaseAdmin from "firebase-admin";
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY ?? ""),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY ?? "",
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
